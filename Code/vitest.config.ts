00483ac13714a0f35f56ec1f743b7ec957971be6
josue-balbontin <balbontinjosue@gmail.com>
2026-05-22 19:39:49 -0400

    configurando vitest

--- Code/vitest.config.ts
+++ Code/vitest.config.ts
@@ -8,5 +8,10 @@ export default defineConfig(({ mode }) => ({
     environment: 'jsdom',
     setupFiles: ['src/test.setup.ts'],
     include: ['src/**/*.spec.ts'],
+    coverage: {
+      provider: 'v8',
+      include: ['src/**/*.ts'],
+      exclude: ['src/**/*.spec.ts', 'src/main.ts', 'src/environments/**', 'src/app/services/supabase/**', 'src/app/acceptance.spec.ts', 'src/app/components/error/**', 'src/app/components/loading/**']
+    }
   },
 }));
